4f430ea805addac0349b8e8e4882c955b3c22662
Solorio17 <jasonsoloriom@gmail.com>
2017-09-02 15:09:19 -0600

    Requiring modules, setting up body-parser with json, and adding a port

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express"),
+      path    = require("path"),
+      bodyParser = require("body-parser");
+
+//INIT APP
+const app = express();
+
+//PORT
+const port = 3000;
+
+//BODY PARSER MIDDLEWARE
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, "public")));
+
+//VIEW SETUP
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.listen(port, () => {
+  console.log("Server Running on port... "+port);
+});
