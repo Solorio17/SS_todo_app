a720b56c4c41090aeed9c30a406c4ad39d81e50a
Solorio17 <jasonsoloriom@gmail.com>
2017-09-02 15:29:17 -0600

    Adding the first get route

--- app.js
+++ app.js
@@ -4,18 +4,19 @@ const express = require("express"),
 
 //INIT APP
 const app = express();
-
 //PORT
 const port = 3000;
-
 //BODY PARSER MIDDLEWARE
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, "public")));
-
 //VIEW SETUP
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 app.listen(port, () => {
   console.log("Server Running on port... "+port);
